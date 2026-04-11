d5cfe69bfcb6bb3aa111f030a6c7d79a1d748118
Kaif <kaifarif508@gmail.com>
2026-04-11 05:39:15 -0700

    fix main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { DarkModeProvider } from './Context/DarkMode.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
